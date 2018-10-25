478cafd082155f2c826ded16fbd4451699729e99
oratosquilla-oratoria <15127577+oratosquilla-oratoria@users.noreply.github.com>
2018-10-25 03:07:28 +0300

    Changed the module docstring, arguments parsing; some refactoring

--- vk_photos.py
+++ vk_photos.py
@@ -1,34 +1,39 @@
-"""Download the photos from a user, group vk.com page or any particular album.
+"""usage: vk_photos.py [-h] [-m] [-s] [-sw] [-sp] [-ss] [-t]
+                    username password path {album,group,user} link
 
-usage: vk_photos.py [-h] [-a ALBUM] [-u USER_PAGE] [-g GROUP_PAGE] [-m] [-s]
-                    [-sw] [-sp] [-ss] [-t]
-                    username password path
+Download the photos from a user, group vk.com page or any particular album.
 
 positional arguments:
-    username                                 VK login
-    password                                 VK password
-    path                                     directory where you want to save the photos
+  username              VK login
+  password              VK password
+  path                  directory where you want to save the photos
+  {album,group,user}    page type
+  link                  link to an album, user page or group page (e.g.
+                        https://vk.com/album11111_111111111 or
+                        https://vk.com/id1)
 
 optional arguments:
-    -h, --help                               show this help message and exit
-    -a ALBUM, --album ALBUM                  link to an album page
-                                             (e.g. https://vk.com/album11111_111111111)
-    -u USER_PAGE, --user_page USER_PAGE      link to a user page (e.g. https://vk.com/id1)
-    -g GROUP_PAGE, --group_page GROUP_PAGE   link to a group page (e.g. https://vk.com/club1)
-    -m, --main                               download the group/user albums
-    -s, --system_all                         download the system albums
-    -sw, --system_wall                       download the photos from the wall (system album)
-    -sp, --system_profile                    download the profile photos (system album)
-    -ss, --system_saved                      download the saved photos (system album)
-    -t, --tagged                             download the photos the user is tagged on
+  -h, --help            show this help message and exit
+  -m, --main            download the group/user albums
+  -s, --system_all      download the system albums
+  -sw, --system_wall    download the photos from the wall (system album)
+  -sp, --system_profile
+                        download the profile photos (system album)
+  -ss, --system_saved   download the saved photos (system album)
+  -t, --tagged          download the photos the user is tagged on
 
+You'll get all the photos from the page if you don't use any positional
+arguments.
 
 For example, to get all the photos from a user page (https://vk.com/id1) (all the albums,
-including system and 'tagged') and save them in 'C:\photos' (your VK login is 'mail@mail.com'
-and VK password is 'mypass'):
-
->> python vk_photos.py mail@mail.com mypass C:\photos -u https://vk.com/id1 -m -s -t
-
+including system and 'tagged') and save them in 'C:\photos' (let 'mail@mail.com' and 'mypass'
+be your login and password):
+>> python vk_photos.py mail@mail.com mypass C:\photos user https://vk.com/id1
+or
+>> python vk_photos.py mail@mail.com mypass C:\photos user https://vk.com/id1 -m -s -t
+These two are the same.
+If you want to get, for instance, only the wall photos and 'tagged', then:
+>> python vk_photos.py mail@mail.com mypass C:\photos user https://vk.com/id1 -sw -t
 """
 
 import argparse
@@ -41,7 +46,8 @@ import requests
 import vk_api
 
 
-def download_album(connection, output_path, owner_id, album_title, album_size, album_id=0, tagged=True):
+def download_album(connection, output_path, owner_id,
+                   album_title, album_size, album_id=0, tagged=True):
     """Create a directory and get an album with VK API.
 
     Parameters:
@@ -178,9 +184,10 @@ def arg_handler(args):
     Parameters:
     -----------
     :args: argparse.Namespace object;
-    :returns: dictionary {'username':      str,  VK login, 'password': str, VK password,
+    :returns: dictionary {'username':      str,  VK login,
+                          'password':      str,  VK password,
                           'output_dir':    str,  directory to save photos in,
-                          'page':          str,  'album' or 'user' or 'group',
+                          'page_type':     str,  'album' or 'user' or 'group',
                           'link':          str,  link to a album/user/group page,
                           'main':          bool, group/user albums,
                           'system_wall':   bool, photos from the wall,
@@ -189,62 +196,37 @@ def arg_handler(args):
                           'tagged':        bool, photos the user is tagged on}
     """
 
-    params = {'username': args.username,
-              'password': args.password,
-              'output_dir': args.path}
-
-    if not args.album and not args.user_page and not args.group_page:
-        raise Exception("There is no page/album link")
-
-    main_system_chosen = (args.main or args.system_all or args.system_profile or
-                          args.system_saved or args.system_wall or args.tagged)
-
-    if ((args.user_page and not (main_system_chosen or args.tagged)) or
-            (args.group_page and not main_system_chosen)):
-        raise Exception("Choose album types to download")
-
-    if args.tagged and not args.user_page:
-        raise Exception("Cannot download 'tagged' photos for an album or a group")
-
-    if args.album and not args.user_page and not args.group_page:
-        params['page'] = 'album'
-        params['link'] = args.album
-    elif not args.album and args.user_page and not args.group_page:
-        params['page'] = 'user'
-        params['link'] = args.user_page
-    elif not args.album and not args.user_page and args.group_page:
-        params['page'] = 'group'
-        params['link'] = args.group_page
-    else:
-        raise Exception("Choose just one page type")
-
-    params['main'] = args.main
-    params['tagged'] = args.tagged
-    params['system_wall'] = args.system_wall
-    params['system_profile'] = args.system_profile
-    params['system_saved'] = args.system_saved
+    params = {'username': args.username, 'password': args.password,
+              'output_dir': args.path, 'page_type': args.page_type,
+              'link': args.link, 'main': args.main, 'tagged': args.tagged,
+              'system_wall': args.system_wall, 'system_profile': args.system_profile,
+              'system_saved': args.system_saved}
 
     if args.system_all:
-        params['system_wall'] = True
-        params['system_profile'] = True
-        params['system_saved'] = True
+        for k in ['system_wall', 'system_profile', 'system_saved']:
+            params[k] = True
+
+    if not (args.main or args.system_all or args.system_profile or
+            args.system_saved or args.system_wall or args.tagged):
+        for k in ['main', 'tagged', 'system_wall', 'system_profile', 'system_saved']:
+            params[k] = True
 
     return params
 
-def link_parse(link, page):
+def link_parse(link, page_type):
     """Returns the owner and album ids for an album or the screen name of either
     user or group page.
 
     Parameters:
     -----------
     :link: str, e.g. https://vk.com/album11111_111111111 or https://vk.com/club1;
-    :page: str, one of {'album', 'user', 'group'};
+    :page_type: str, one of {'album', 'user', 'group'};
     :returns: str or tuple of ints, e.g. 'club1' or (11111, 111111111)
     """
 
     link_end = link.split('/')[-1]
 
-    if page == 'album':
+    if page_type == 'album':
         ids = link_end.split('album')[1].split('_')
         return int(ids[0]), int(ids[1])
     else:
@@ -260,17 +242,16 @@ if __name__ == '__main__':
 
     # Parse the arguments
     parser = argparse.ArgumentParser(
-        description="Download the photos from a user, group vk.com page or album."
+        description="Download the photos from a user, group vk.com page or any particular album.",
+        epilog="You'll get all the photos from the page if you don't use any positional arguments."
     )
     parser.add_argument('username', help="VK login")
     parser.add_argument('password', help="VK password")
     parser.add_argument('path', help="directory where you want to save the photos")
-    parser.add_argument('-a', '--album',
-                        help="link to an album page (e.g. https://vk.com/album11111_111111111)")
-    parser.add_argument('-u', '--user_page',
-                        help="link to a user page (e.g. https://vk.com/id1)")
-    parser.add_argument('-g', '--group_page',
-                        help="link to a group page (e.g. https://vk.com/club1)")
+    parser.add_argument('page_type', choices=['album', 'group', 'user'], help="page type")
+    parser.add_argument('link', help="""link to an album, user page or group page
+                                        (e.g. https://vk.com/album11111_111111111 or
+                                        https://vk.com/id1)""")
     parser.add_argument('-m', '--main', action='store_true', default=False,
                         help="download the group/user albums")
     parser.add_argument('-s', '--system_all', action='store_true', default=False,
@@ -294,16 +275,16 @@ if __name__ == '__main__':
         connection = vk_api.VkApi(args_dict['username'], args_dict['password'])
         connection.auth()
 
-        if args_dict['page'] == 'album':
-            owner_id, album_id = link_parse(args_dict['link'], args_dict['page'])
+        if args_dict['page_type'] == 'album':
+            owner_id, album_id = link_parse(args_dict['link'], args_dict['page_type'])
             album_size = get_album_size(connection, owner_id, album_id, False)
             print('\n{}. Num of photos: {}.'.format('Album', album_size))
             download_album(connection, args_dict['output_dir'], owner_id,
                            'Album', album_size, album_id, False)
         else:
-            screen_name = link_parse(args_dict['link'], args_dict['page'])
+            screen_name = link_parse(args_dict['link'], args_dict['page_type'])
 
-            if args_dict['page'] == 'user':
+            if args_dict['page_type'] == 'user':
                 owner_id = get_owner_id(connection, screen_name, True)
             else:
                 owner_id = get_owner_id(connection, screen_name, False)
@@ -323,10 +304,13 @@ if __name__ == '__main__':
                 except vk_api.exceptions.ApiError as vk_e:
                     print('\n(--main) album', vk_e)
 
-            if args_dict['page'] == 'user' and args_dict['tagged']:
+            if args_dict['page_type'] == 'user' and args_dict['tagged']:
                 try:
                     album_size = get_album_size(connection, owner_id)
-                    print('\n{}. Num of photos: {}.'.format('Photos the user is tagged on', album_size))
+                    print('\n{}. Num of photos: {}.'.format(
+                        'Photos the user is tagged on',
+                        album_size
+                    ))
                     download_album(connection, args_dict['output_dir'], owner_id,
                                    'Photos the user is tagged on', album_size)
 
@@ -376,10 +360,10 @@ if __name__ == '__main__':
         sys.exit()
 
     except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        exc_type, exc_obj, exc_trace_back = sys.exc_info()
+        file_name = os.path.split(exc_trace_back.tb_frame.f_code.co_filename)[1]
         print(e)
-        print(exc_type, fname, exc_tb.tb_lineno)
+        print(exc_type, file_name, exc_trace_back.tb_lineno)
         sys.exit(1)
 
     finally:
